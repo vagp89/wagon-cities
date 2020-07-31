331a3e826e010cebcec34464e29b5464ad3b534b
Victoria Gonzales <vagp89@gmail.com>
2020-07-31 17:03:50 +0200

    redux practice app how to access to the store

--- src/containers/ActiveCity.jsx
+++ src/containers/ActiveCity.jsx
@@ -4,11 +4,12 @@ import { connect } from 'react-redux';
 
 class ActiveCity extends Component {
   render() {
+    const url = `https://kitt.lewagon.com/placeholder/cities/${this.props.city.slug}`
     return (
       <div className="col-sm-7">
         <h1>{this.props.city.name}</h1>
         <p>{this.props.city.address}</p>
-         <img src={"https://kitt.lewagon.com/placeholder/cities/paris"} className="image"/>
+         <img src={url} className="image"/>
       </div>
     );
   }
